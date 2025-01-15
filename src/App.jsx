b65cccd082c9d335f3dbec471ad71310dc61d798
LorenzoScalvini <lorenzo.scalvini1704@gmail.com>
2025-01-15 17:21:27 +0100

    ADD: styling list

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import MainPage from './pages/MainPage/MainPage'
-import ShowPage from './pages/ShowPage/ShowPage'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import MainPage from "./pages/MainPage/MainPage";
+import ShowPage from "./pages/ShowPage/ShowPage";
+import TravelList from "./components/TravelList";
 
 function App() {
-
   return (
     <>
       <BrowserRouter>
+        <TravelList />
         <Routes>
           <Route>
             <Route index element={<MainPage />} />
-            <Route path='/travel/:id' element={<ShowPage />} />
+            <Route path="/travel/:id" element={<ShowPage />} />
           </Route>
         </Routes>
       </BrowserRouter>
-
     </>
-  )
+  );
 }
 
-export default App
+export default App;
